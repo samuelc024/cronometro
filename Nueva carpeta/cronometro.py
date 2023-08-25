5ad8c81bc5e76a416bf6823b56d68e6b32d522b2
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-08-25 06:08:05 -0500

    Update cronometro.py

--- Nueva carpeta/cronometro.py
+++ Nueva carpeta/cronometro.py
@@ -1,14 +1,14 @@
 from hora import Hora 
 from minuto import Minuto 
 from segundo import Segundo 
-
+import time 
 class Cronometro():
     def __init__(self):
         self.hora=Hora()
         self.minuto=Minuto()
         self.segundo=Segundo()
     def avanzar(self):
-        
+        time.sleep(1)
         self.segundo.avanzar()
         if self.segundo.valor ==0:
             self.minuto.avanzar()
