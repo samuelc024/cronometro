24f59def5b3b2c0b9c71be3d88e01dfe59f12cd0
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-08-25 06:06:04 -0500

    Update principal.py

--- Nueva carpeta/principal.py
+++ Nueva carpeta/principal.py
@@ -1,7 +1,35 @@
 from cronometro import Cronometro
 
-c = Cronometro()
-
-for i in range(999):
-    print(c.hora.valor ,c.minuto.valor, c.segundo.valor )
+c= Cronometro()
+h,m,s=str(input("Ingrese las horas, minutos y segundos que desea contar respectivamente separados por un espacio (hh mm ss):\n")).split(" ")
+h=int(h)
+m=int(m)
+s=int(s)
+s= s + (h*3600) + (m*60)
+input("\nPresione enter para iniciar el conteo...")
+for i in range(s+1):
+    if c.segundo.valor//10==0:
+        if c.minuto.valor//10==0:
+            if c.hora.valor//10==0:
+                print("0"+str(c.hora.valor)+":0"+str(c.minuto.valor)+":0"+str(c.segundo.valor))
+            else:
+                print(str(c.hora.valor)+":0"+str(c.minuto.valor)+":0"+str(c.segundo.valor))
+        else:
+            if c.hora.valor//10==0:
+                print("0"+str(c.hora.valor)+":"+str(c.minuto.valor)+":0"+str(c.segundo.valor))
+            else:
+                print(str(c.hora.valor)+":"+str(c.minuto.valor)+":0"+str(c.segundo.valor))
+    else:
+        if c.minuto.valor//10==0:
+            if c.hora.valor//10==0:
+                print("0"+str(c.hora.valor)+":0"+str(c.minuto.valor)+":"+str(c.segundo.valor))
+            else:
+                print(str(c.hora.valor)+":0"+str(c.minuto.valor)+":"+str(c.segundo.valor))
+        else:
+            if c.hora.valor//10==0:
+                print("0"+str(c.hora.valor)+":"+str(c.minuto.valor)+":"+str(c.segundo.valor))
+            else:
+                print(str(c.hora.valor)+":"+str(c.minuto.valor)+":"+str(c.segundo.valor))
     c.avanzar()
+print("\n\tCONTEO FINALIZADO\n")
+print("\t◥------◥\n\tl ● ▄ ◉ l ѠOOƑ!\n\tl‿/ʊ\‿l\n\tl══o══l\n\t︳ ︳︳ l⊃\n\tఋ︵ ఋ\n")
