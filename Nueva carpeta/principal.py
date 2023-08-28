2c72aab1477431699bbdec8615b75f5e64e3abe1
samuelc024 <142014521+samuelc024@users.noreply.github.com>
2023-08-28 05:32:20 -0500

    Actualizar principal.py

--- Nueva carpeta/principal.py
+++ Nueva carpeta/principal.py
@@ -30,6 +30,5 @@ for i in range(s+1):
                 print("0"+str(c.hora.valor)+":"+str(c.minuto.valor)+":"+str(c.segundo.valor))
             else:
                 print(str(c.hora.valor)+":"+str(c.minuto.valor)+":"+str(c.segundo.valor))
-    c.avanzar()
+    
 print("\n\tCONTEO FINALIZADO\n")
-print("\t◥------◥\n\tl ● ▄ ◉ l ѠOOƑ!\n\tl‿/ʊ\‿l\n\tl══o══l\n\t︳ ︳︳ l⊃\n\tఋ︵ ఋ\n")
